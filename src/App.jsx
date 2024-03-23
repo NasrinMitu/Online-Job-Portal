4ef56a29ab043ab2227bc19ffb81c177d1a36710
shamima <nasrin.shamima32@gmail.com>
2024-03-23 17:40:55 +0600

    First Commit of Online Job Portal

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import SearchBar from "./components/SearchBar";
+
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Header/>
+      <SearchBar/>
+
+    </div>
+  )
+}
+
+export default App
